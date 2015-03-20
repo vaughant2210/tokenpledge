5416d794283774e9b84c351201a88ce552dbe8e0
Vaughan Thandroyen <vaughant2210@gmail.com>
2015-03-20 09:43:44 -0400

    project baseline

--- app/adapters/nutritionix.js
+++ app/adapters/nutritionix.js
@@ -1,14 +1,4 @@
-import ajax from 'ic-ajax';
-import Ember from 'ember';
+import ApplicationAdapter from './application';
 
-export default Ember.Object.extend({
-  findAll: function(subreddit) {
-   return ajax("https://api.nutritionix.com/v1_1/search" + "/.json?jsonp=?").then(
-     function(response) {
-       return response.data.children.map(function (child) {
-         return App.RedditLink.create(child.data);
-       });
-     }
-   );
- }
+export default ApplicationAdapter.extend({
 });
